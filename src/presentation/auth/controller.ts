52c4444cd625601f5475174af21faa7b1115b18c
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2024-03-13 22:11:36 -0400

    Arreglando auth

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -12,8 +12,8 @@ export class AuthController {
 
      loginSchema = yup.object({
 
-        email: yup.string().email().required(),
-        password: yup.string().min(6).required(),
+        username: yup.string().required(),
+        idDiscord: yup.string().min(6).required(),
     
     })
 
@@ -22,19 +22,19 @@ export class AuthController {
 
             const requestData = await req.body;
             
-            const { email, password} = await this.loginSchema.validate({
+            const { username, idDiscord} = await this.loginSchema.validate({
                 ...requestData,
             });
     
     
-            const user = await prisma.user.findFirst({where: {email}});
+            const user = await prisma.user.findFirst({where: {username}});
     
             if(!user){
-                return res.status(400).json({message: `El ${email} no existe en la Base de Datos`});
+                return res.status(400).json({message: `El ${username} no existe en la Base de Datos`});
             }
     
             // Verificar la contraseña
-            const validPassword = await bcrypt.compareSync(password, user.password);
+            const validPassword = await bcrypt.compareSync(idDiscord, user.idDiscord);
                 if (!validPassword) {
                     return res.status(400).json({message: 'Password incorrecto  ' });
                     }
@@ -49,28 +49,27 @@ export class AuthController {
 
     registerSchema = yup.object({
         name: yup.string().required(),
-        email: yup.string().email().required(),
-        password: yup.string().min(6).required(),
+        username: yup.string().required(),
+        idDiscord: yup.string().min(6).required(),
         idrol: yup.number().required(),
     })
 
     public register = async (req:Request, res:Response) => {
         try {
             const requestData = await req.body;
-            const { name, email, password, idrol } = await this.registerSchema.validate({
+            const { name, username, idDiscord, idrol } = await this.registerSchema.validate({
                 ...requestData,
                 
             });
     
             // Encripta la contraseña antes de guardarla en la base de datos
-            const hashedPassword = await bcrypt.hash(password, 10); // El segundo argumento es el número de rondas de hash
+            const hashedPassword = await bcrypt.hash(idDiscord, 10); // El segundo argumento es el número de rondas de hash
     
             const user = await prisma.user.create({
 
                 data: {
-                    name,
-                    email,
-                    password: hashedPassword,
+                    username,
+                    idDiscord: hashedPassword,
                     idrol,
                 }
             });
